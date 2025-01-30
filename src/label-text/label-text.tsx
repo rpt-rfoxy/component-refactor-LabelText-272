de42a107979420c3bb67a8b3b19bebd04ba4fdd2
Ridwan Foxdream (foxy) <rfoxdream+github@reapit.com>
2025-01-30 02:14:31 +0000

    update text-label

--- src/label-text/label-text.tsx
+++ src/label-text/label-text.tsx
@@ -1,19 +1,27 @@
-import React, { FC, LabelHTMLAttributes } from 'react'
-import { elLabelText } from './styles'
+import React, { FC, LabelHTMLAttributes } from "react";
+import { elLabelText } from "./styles";
 
 export interface LabelTextProps extends LabelHTMLAttributes<HTMLLabelElement> {
-  variant?: 'soft' | 'strong'
-  size?: 'small' | 'medium'
-  legend?: 'label' | 'span' | 'text'
-  isRequired?: boolean
+  variant?: "soft" | "strong";
+  size?: "small" | "medium";
+  legend?: "label" | "span" | "text" | "dt" | "p";
+  isRequired?: boolean;
 }
 
-export const LabelText: FC<LabelTextProps> = ({ children, isRequired, size = 'medium', variant = 'soft', legend = "label", className, ...rest }) => {
-  const ElLabelText = elLabelText(legend)
+export const LabelText: FC<LabelTextProps> = ({
+  children,
+  isRequired,
+  size = "medium",
+  variant = "soft",
+  legend = "label",
+  className,
+  ...rest
+}) => {
+  const ElLabelText = elLabelText(legend);
   return (
     <ElLabelText {...rest} data-size={size} data-variant={variant}>
       {children}
-      {isRequired && ' *'}
+      {isRequired && " *"}
     </ElLabelText>
-  )
-}
+  );
+};
